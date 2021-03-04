74d84a3d3d5cec329300c7389e3b84a7eb58af19
Ignacio Martinez <50220027+rocogamer@users.noreply.github.com>
2021-03-04 12:31:04 +0100

    GetUsers update

--- html/API/admintools/getUsers.php
+++ html/API/admintools/getUsers.php
@@ -1,7 +1,12 @@
 <?php
     include('../../../phplibraries/database.php');
     $con = mysqli_connect(DBHOST, DBUSR, DBPASSWD, DBNAME) or die('Unable To connect');
-    $result = mysqli_query($con,"SELECT * FROM login_data");
+    if (count($_GET)>0) {
+        $result = mysqli_query($con,"SELECT * FROM login_data WHERE ID>".$_GET['i']);
+    } else {
+        $result = mysqli_query($con,"SELECT * FROM login_data");
+        $lastid = 0;
+    }
     if(mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result)) {
             echo "<tr>";
@@ -13,7 +18,9 @@
             echo "<td>" . $row["permission_users"] . "</td>";
             echo "<td> <button class=\"btn btn-success\" type=\"button\" onclick=\"alert('WIP')\"><i class=\"bi bi-pencil-square\"></i>Editar</button> </td>";
             echo "</tr>";
+            $lastid++;
         }
+        $_SESSION['UserLastId'] = $lastid;
     } else {
         echo "<tr>";
         echo "<td colspan=7> No users </td>";
--- html/js/admin.js
+++ html/js/admin.js
@@ -1,24 +1,36 @@
-function loadLogs($table, $autorizationkey) {
-    var xhttp;
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
+function firstLoadLogs($table, $autorizationkey) {
+  var xhttp;
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById($table).innerHTML += this.responseText;
+    }
+  };
+  xhttp.open("GET", "../API/admintools/getLog.php", true);
+  xhttp.setRequestHeader("Authorization", "Basic " + $autorizationkey);
+  xhttp.send();
+}
+function firstLoadUsers($table, $autorizationkey) {
+  var xhttp;
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
       document.getElementById($table).innerHTML += this.responseText;
-      }
-    };
-    xhttp.open("GET", "../API/admintools/getLog.php", true);
-    xhttp.setRequestHeader("Authorization", "Basic " + $autorizationkey);
-    xhttp.send();
-  }
-  function loadUsers($table, $autorizationkey) {
-    var xhttp;
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-      document.getElementById($table).innerHTML += this.responseText;
-      }
-    };
-    xhttp.open("GET", "../API/admintools/getUsers.php", true);
-    xhttp.setRequestHeader("Authorization", "Basic " + $autorizationkey);
-    xhttp.send();
-  }
+    }
+  };
+  xhttp.open("GET", "../API/admintools/getUsers.php", true);
+  xhttp.setRequestHeader("Authorization", "Basic " + $autorizationkey);
+  xhttp.send();
+}
+function newLoadUsers($table, $autorizationkey, $lastIdUsers) {
+  var xhttp;
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById($table).innerHTML += this.responseText;
+    }
+  };
+  xhttp.open("GET", "../API/admintools/getUsers.php?i="+$lastIdUsers, true);
+  xhttp.setRequestHeader("Authorization", "Basic " + $autorizationkey);
+  xhttp.send();
+}
