db5c03de09372e84e641264ef555ad8c846e75f0
Ignacio Martinez <50220027+rocogamer@users.noreply.github.com>
2021-03-04 15:54:43 +0100

    Commented a little bugged feature

--- html/admin/index.php
+++ html/admin/index.php
@@ -27,9 +27,9 @@
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
       <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Biblioteca Virtual</a>
       <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <p>Sesion iniciada con: <?php echo $_SESSION["name"]; ?></p>
-        </li>
+        <!--<li class="nav-item text-nowrap">
+          <p>Sesion iniciada con: <?php /*echo $_SESSION["name"];*/ ?></p>
+        </li>-->
         <li class="nav-item text-nowrap">
           <a class="nav-link" href="logout.php">Cerrar sesion</a>
         </li>
