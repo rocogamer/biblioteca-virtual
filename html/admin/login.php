12b1c4fccc0ea12426fc4703d3da10838cdad760
Ignacio Martinez <50220027+rocogamer@users.noreply.github.com>
2021-03-04 03:12:00 +0100

    Login system and logout system update

--- html/admin/login.php
+++ html/admin/login.php
@@ -1,20 +1,20 @@
 <!-- Sistema de contraseña olvidada -->
 <?php
     include '../../phplibraries/database.php';
-    /* 
     session_start();
     if(count($_POST)>0) {
         $con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWD,$DB_DB) or die('Unable To connect');
-        $result = mysqli_query($con,"");
+        $result = mysqli_query($con,"SELECT ID,name,username FROM login_data WHERE username='" . htmlspecialchars($_POST['username']) . "' and password='" . md5(htmlspecialchars($_POST['password'])) . "'");
         $row  = mysqli_fetch_array($result);
         if(is_array($row)) {
             $_SESSION["ID"] = $row['id'];
             $_SESSION["name"] = $row['name'];
+            $_SESSION["username"] = $row['username'];
         } else {
          $message = "Invalid Username or Password!";
         }
     }
     if (isset($_SESSION['ID'])) {
         Header('Location:admin.php');
-    } */
+    } 
 ?>
