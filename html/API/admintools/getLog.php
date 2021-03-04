80afed9578b0f1902348fa9a8d64933c316b5e0e
Ignacio Martinez <50220027+rocogamer@users.noreply.github.com>
2021-03-04 04:34:46 +0100

    getLog API and typo updates

---
+++ html/API/admintools/getLog.php
@@ -0,0 +1,13 @@
+<?php
+    include '../../../phplibraries/database.php';
+    $con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWD,$DB_DB) or die('Unable To connect');
+    $result = mysqli_query($con,"SELECT * FROM log_actions");
+    if(mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "<td> " . $row["ID"]. "</td> <td>" . $row["action"]. "</td> <td>" . $row["user"]. "</td> <td>" . $row["date"] . "</td>";
+        }
+    } else {
+       echo "No logs";
+    }
+
+?>
--- html/admin/login.php
+++ html/admin/login.php
@@ -4,7 +4,7 @@
     session_start();
     if(count($_POST)>0) {
         $con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWD,$DB_DB) or die('Unable To connect');
-        $result = mysqli_query($con,"SELECT ID,name,username,permission_users,permission_books,permission_categories FROM login_data WHERE username='" . htmlspecialchars($_POST['username']) . "' and password='" . md5(htmlspecialchars($_POST['password'])) . "'");
+        $result = mysqli_query($con,"SELECT ID,name,username,permission_users,permission_books,permission_categories FROM login_data WHERE username='" . htmlspecialchars($_POST["username"]) . "' and password='" . md5(htmlspecialchars($_POST["password"])) . "'");
         $row  = mysqli_fetch_array($result);
         if(is_array($row)) {
             $_SESSION["ID"] = $row['id'];
