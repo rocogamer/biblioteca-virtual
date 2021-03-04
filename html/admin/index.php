0b200abc85c73214b844a436b8958e107a547c81
Ignacio Martinez <50220027+rocogamer@users.noreply.github.com>
2021-03-04 15:52:58 +0100

    change color button

--- html/admin/index.php
+++ html/admin/index.php
@@ -145,7 +145,7 @@
                         </div>
                       </div>
                       <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-info" data-bs-dismiss="modal">Close</button>
                       </div>
                     </div>
                   </div>
