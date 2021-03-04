67ba372a85df62dc6ed62072aff6f6d2830c2a15
Ignacio Martinez <50220027+rocogamer@users.noreply.github.com>
2021-03-04 06:01:01 +0100

    Security improvement in removeUser

--- html/API/admintools/removeUser.php
+++ html/API/admintools/removeUser.php
@@ -3,8 +3,8 @@
     session_start();
     if(count($_POST)>0) {
         $con = mysqli_connect(DBHOST, DBUSR, DBPASSWD, DBNAME) or die('Unable To connect');
-        if(mysqli_query($con, "DELETE FROM login_data WHERE ID='" . (int)$_POST["UserID"] ."'")) {
-            $resultlog = mysqli_query($con, "INSERT INTO `log_actions` (`action`, `user`, `date`) VALUES ('Se ha eliminado el usuario " . $_POST["UserName"] . ".', ' " . $_SESSION["username"] . " ', '" . date("Y/m/d H/i/s") . "'");
+        if(mysqli_query($con, "DELETE FROM login_data WHERE ID='" . (int)$_POST["userID"] . "' AND password='" . $_POST["pwd"] . "' AND username='" . $_POST["username"] . "'")) {
+            $resultlog = mysqli_query($con, "INSERT INTO `log_actions` (`action`, `user`, `date`) VALUES ('Se ha eliminado el usuario " . $_POST["username"] . ".', ' " . $_SESSION["username"] . " ', '" . date("Y/m/d H/i/s") . "'");
             echo "User deleted";
         } else {
             echo "Delete failed";
